506b2d6aa311a88d2ab0d4c731a0485e27226629
Ravi prasad reddy <raviprasad.aenugu@gmail.com>
2020-09-08 07:47:27 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
+import { BillingComponent } from './billing/billing.component';
+import { SupervisordetailsComponent } from './supervisordetails/supervisordetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductsComponent,
+    BillingComponent,
+    SupervisordetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/supervisordetails/supervisordetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supervisordetails',
+  templateUrl: './supervisordetails.component.html',
+  styleUrls: ['./supervisordetails.component.css']
+})
+export class SupervisordetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
